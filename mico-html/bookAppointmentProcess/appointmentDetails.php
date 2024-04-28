11a33e7f27d8b5fae58b88b71da8e06759fbbc47
Ani <a.usanetashvili1@gmail.com>
2024-04-28 17:03:30 +0400

    complete confirmation page

--- mico-html/bookAppointmentProcess/appointmentDetails.php
+++ mico-html/bookAppointmentProcess/appointmentDetails.php
@@ -89,25 +89,3 @@
   <!-- end book confirmation -->
 
 
-  <!-- footer section -->
-  <?php include '../categories/info.php';?>
-  <?php include './footer.php';?>
-  <!-- end footer section -->
-
-  <!-- jQery -->
-  <script src="js/jquery-3.4.1.min.js"></script>
-  <!-- bootstrap js -->
-  <script src="js/bootstrap.js"></script>
-  <!-- nice select -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/js/jquery.nice-select.min.js" integrity="sha256-Zr3vByTlMGQhvMfgkQ5BtWRSKBGa2QlspKYJnkjZTmo=" crossorigin="anonymous"></script>
-  <!-- owl slider -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-  <!-- datepicker -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.js"></script>
-  <!-- custom js -->
-  <script src="js/custom.js"></script>
-
-
-</body>
-
-</html>
